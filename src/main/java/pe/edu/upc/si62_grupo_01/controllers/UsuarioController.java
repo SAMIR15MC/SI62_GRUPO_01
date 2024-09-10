0963dd9821337f3adf53a58057d393239cb6c0bc
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-09-10 11:57:57 -0500

    correcciones
    correcciones

--- src/main/java/pe/edu/upc/si62_grupo_01/controllers/UsuarioController.java
+++ src/main/java/pe/edu/upc/si62_grupo_01/controllers/UsuarioController.java
@@ -3,9 +3,7 @@ package pe.edu.upc.si62_grupo_01.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.si62_grupo_01.dtos.TerrenoDTO;
 import pe.edu.upc.si62_grupo_01.dtos.UsuarioDTO;
-import pe.edu.upc.si62_grupo_01.entities.Terreno;
 import pe.edu.upc.si62_grupo_01.entities.Usuario;
 import pe.edu.upc.si62_grupo_01.servicesinterfaces.IUsuarioService;
 
@@ -15,31 +13,34 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
+
     @Autowired
     private IUsuarioService uS;
 
     @PostMapping
     private void insertar(@RequestBody UsuarioDTO dto) {
         ModelMapper m= new ModelMapper();
-        Usuario usuario= m.map(dto, Usuario.class);
+        Usuario usuario = m.map(dto, Usuario.class);
         uS.insert(usuario);
     }
+
     @GetMapping
     public List<UsuarioDTO> listar() {
-        return uS.list().stream().map(x->
-        {
-            ModelMapper m= new ModelMapper();
-            return m.map(x,UsuarioDTO.class);
+        return uS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, UsuarioDTO.class);
         }).collect(Collectors.toList());
     }
+
     @PutMapping
-    public void modificar(@RequestBody UsuarioDTO usuarioDTO){
-        ModelMapper m= new ModelMapper();
-        Usuario u= m.map(usuarioDTO,Usuario.class);
+    public void modificar(@RequestBody UsuarioDTO usuarioDTO) {
+        ModelMapper m = new ModelMapper();
+        Usuario u = m.map(usuarioDTO, Usuario.class);
         uS.update(u);
     }
-    @DeleteMapping
-    public void eliminar(@PathVariable ("id")Integer id){
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Long id) { // Cambiado de Integer a Long
         uS.delete(id);
     }
 }
