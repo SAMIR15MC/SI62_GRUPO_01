20121656e10760f2971e75489c3b00b707db6685
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-09-09 23:19:24 -0500

    Update ComentarioController.java

--- src/main/java/pe/edu/upc/si62_grupo_01/controllers/ComentarioController.java
+++ src/main/java/pe/edu/upc/si62_grupo_01/controllers/ComentarioController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/comentario")
+@RequestMapping("/hola")
 public class ComentarioController {
     @Autowired
     private IComentarioService comentarioService;
